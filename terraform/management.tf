a91b6dd2ad76e28a5edcc86ac74b8b581b3f917d
Michael Daniel Naguib <mdanielnaguib@deloitte.it>
2025-04-15 15:02:00 +0200

    fix: update OU

--- terraform/management.tf
+++ terraform/management.tf
@@ -8,7 +8,7 @@ module "management" {
     AccountEmail = "aft_training@yopmail.com"
     AccountName  = "aft_training"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "root"
+    ManagedOrganizationalUnit = "Root"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
     SSOUserEmail     = "do-not-use@example.com"
